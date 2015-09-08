0d7ab8cde676fa572127822320eb6b351d1ca7cb
liyu <liyu871230@163.com>
2015-09-08 13:49:33 +0800

    生产者/消费者模型实现

---
+++ src/TestSynchronized.java
@@ -0,0 +1,36 @@
+
+public class TestSynchronized {
+	class Foo extends Thread {
+		private String name;
+		public Foo(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public void run() {
+			synchronized (Foo.class) {
+				while (true) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					printName();
+				}
+			}
+
+		}
+
+		void printName() {
+			System.out.println(name);
+		}
+	}
+
+	public static void main(String[] args) {
+		TestSynchronized test = new TestSynchronized();
+		Foo f1 = test.new Foo("li");
+		Foo f2 = test.new Foo("yin");
+		f1.start();
+		f2.start();
+	}
+}
